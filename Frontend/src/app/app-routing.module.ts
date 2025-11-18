4c796558e55b4bc326816a06bab0039f7ed69f45
Fabian Sakowski <103035840+fabi0167@users.noreply.github.com>
2025-11-18 12:10:40 +0100

    Add dashboard item overview filters and status chart

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -34,6 +34,7 @@ import { PartTypeComponent } from './part-type/part-type.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ActiveLoansComponent } from './loan-active/active-loans.component';
 import { StatusItemsComponent } from './dashboard-status/status-items.component';
+import { DashboardItemsComponent } from './dashboard-items/dashboard-items.component';
 
 export const routes: Routes = [
   //Default route
@@ -76,6 +77,12 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [1, 2, 4] },
+  },
+  {
+    path: 'dashboard/items',
+    component: DashboardItemsComponent,
     canActivate: [AuthGuard, RoleGuard],
     data: { roles: [1, 2, 4] },
   },
